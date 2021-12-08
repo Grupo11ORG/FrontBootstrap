5e054583467b5a67b517bf7c49392a5570bcd4e6
G4brielG <gabriel240901@gmail.com>
2021-12-08 14:47:41 -0300

    a

--- src/components/About.js
+++ src/components/About.js
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
+//a
 
 const About = () => {
-    return (
-        <section class="page-section bg-primary" id="about">
-        <div class="container px-4 px-lg-5">
-            <div class="row gx-4 gx-lg-5 justify-content-center">
-                <div class="col-lg-8 text-center">
-                    <h2 class="text-white mt-0">Especialistas tecnologicos, al alcance de tu mano!</h2>
-                    <hr class="divider divider-light" />
-                    <p class="text-white-75 mb-4">Acercamos a aquellos especialistas tecnologicos para que pueda elegir por capacidades y calidad!</p>
-                </div>
-            </div>
+  return (
+    <section class="page-section bg-primary" id="about">
+      <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+          <div class="col-lg-8 text-center">
+            <h2 class="text-white mt-0">
+              Especialistas tecnologicos, al alcance de tu mano!
+            </h2>
+            <hr class="divider divider-light" />
+            <p class="text-white-75 mb-4">
+              Acercamos a aquellos especialistas tecnologicos para que pueda
+              elegir por capacidades y calidad!
+            </p>
+          </div>
         </div>
+      </div>
     </section>
-    )
-}
+  );
+};
 
-export default About
+export default About;
