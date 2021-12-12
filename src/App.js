347af498d09277c439dc8514008700926c8f7548
G4brielG <gabriel240901@gmail.com>
2021-12-12 19:01:43 -0300

    a

--- src/App.js
+++ src/App.js
@@ -12,19 +12,29 @@ import Home from "./pages/Home";
 import PerfilTec from "./pages/PerfilTec";
 import Register from "./pages/Register";
 import Admin from "./pages/Admin";
+import Ficha from "./pages/Ficha";
+
 import { Session } from "./context/Session";
 import { useState } from "react";
 
-
-
 function App() {
-
   const [session, setSession] = useState(null);
   const Routing = () => {
-    
+    if (session != null) {
+      return (
+        <Switch>
+          <Route exact path="/" component={Principal} />
+          {/* <Route exact path="/principal" component={Principal} /> */}
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/perfil" component={PerfilTec} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/registrar" component={Register} />
+          <Route exact path="/admin" component={Admin} />
+        </Switch>
+      );
+    }
 
-      if (session != null){
-        return(
+    return (
       <Switch>
         <Route exact path="/" component={Principal} />
         {/* <Route exact path="/principal" component={Principal} /> */}
@@ -33,24 +43,12 @@ function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/registrar" component={Register} />
         <Route exact path="/admin" component={Admin} />
+        <Route exact path="/ficha/" component={Ficha} />
       </Switch>
-        )};
-  
-  
-
+    );
+  };
   return (
-    <Switch>
-    <Route exact path="/" component={Principal} />
-    {/* <Route exact path="/principal" component={Principal} /> */}
-    <Route exact path="/home" component={Home} />
-    <Route exact path="/perfil" component={PerfilTec} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/registrar" component={Register} />
-    <Route exact path="/admin" component={Admin} />
-  </Switch>
-  )}
-  return (
-    <Session.Provider value= {[session, setSession]}>
+    <Session.Provider value={[session, setSession]}>
       <Router>
         <Routing />
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,21 +12,19 @@ const Navbar = () => {
     // navbar-shrink
 
     <nav
-      class="navbar navbar-expand-lg navbar-light fixed-top py-3 navbar-shrink"
+      className="navbar navbar-expand-lg navbar-light fixed-top py-3 navbar-shrink"
       id="mainNav"
       style={{
         width: "100%",
-        height: "60px"
-
+        height: "60px",
       }}
     >
-    
-      <div class="container px-4 px-lg-5">
-        <Link className="navbar-brand" to="/" style={{fontSize: "25px"   }}>
-          <i class="glyphicon glyphicon-phone"></i>TechSearch
+      <div className="container px-4 px-lg-5">
+        <Link className="navbar-brand" to="/" style={{ fontSize: "25px" }}>
+          <i className="glyphicon glyphicon-phone"></i>TechSearch
         </Link>
         <button
-          class="navbar-toggler navbar-toggler-right"
+          className="navbar-toggler navbar-toggler-right"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarResponsive"
@@ -34,68 +32,117 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span class="navbar-toggler-icon"></span>
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ms-auto my-2 my-lg-0">
+        <div className="collapse navbar-collapse" id="navbarResponsive">
+          <ul className="navbar-nav ms-auto my-2 my-lg-0">
             {session === null ? (
               <>
-                {/* <li class="nav-item"><Link class="nav-link" href="#about">About</Link></li> */}
-                <li class="nav-item" >
-                  <Link class="nav-link" to="/login" style={{fontSize: "15px"}}><i class="glyphicon glyphicon-log-in"></i>
+                {/* <li className="nav-item"><Link className="nav-link" href="#about">About</Link></li> */}
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/login"
+                    style={{ fontSize: "15px" }}
+                  >
+                    <i className="glyphicon glyphicon-log-in"></i>
                     Login
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/registrar" style={{fontSize: "15px"}}><i class="glyphicon glyphicon-new-window"></i>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/registrar"
+                    style={{ fontSize: "15px" }}
+                  >
+                    <i className="glyphicon glyphicon-new-window"></i>
                     Register
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/home" style={{fontSize: "15px"}}><i class="glyphicon glyphicon-home"></i>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/home"
+                    style={{ fontSize: "15px" }}
+                  >
+                    <i className="glyphicon glyphicon-home"></i>
                     Home
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/perfil" style={{fontSize: "15px"}}><i class="glyphicon glyphicon-user"></i> 
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/perfil"
+                    style={{ fontSize: "15px" }}
+                  >
+                    <i className="glyphicon glyphicon-user"></i>
                     PerfilTec
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/admin" style={{fontSize: "15px"}}><i class="glyphicon glyphicon-floppy-disk"></i>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/admin"
+                    style={{ fontSize: "15px" }}
+                  >
+                    <i className="glyphicon glyphicon-floppy-disk"></i>
                     Admin
                   </Link>
                 </li>
               </>
             ) : (
               <>
-                <li class="nav-item">
-                  <a class="nav-link" href="#about" style={{fontSize: "15px"}}>
+                <li className="nav-item">
+                  <a
+                    className="nav-link"
+                    href="#about"
+                    style={{ fontSize: "15px" }}
+                  >
                     About
                   </a>
                 </li>
-                <li class="nav-item">
-                  <Link class="nav-link" to="/login" style={{fontSize: "15px"}}>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/login"
+                    style={{ fontSize: "15px" }}
+                  >
                     Login
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/registrar" style={{fontSize: "15px"}}>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/registrar"
+                    style={{ fontSize: "15px" }}
+                  >
                     Register
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/home" style={{fontSize: "15px"}}>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/home"
+                    style={{ fontSize: "15px" }}
+                  >
                     Home
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/perfil" style={{fontSize: "15px"}}>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/perfil"
+                    style={{ fontSize: "15px" }}
+                  >
                     PerfilTec
                   </Link>
                 </li>
-                <li class="nav-item">
-                  <Link className="nav-link" to="/admin" style={{fontSize: "15px"}}>
+                <li className="nav-item">
+                  <Link
+                    className="nav-link"
+                    to="/admin"
+                    style={{ fontSize: "15px" }}
+                  >
                     Admin
                   </Link>
                 </li>
@@ -108,7 +155,7 @@ const Navbar = () => {
                       localStorage.removeItem("rstoken");
                       history.push("/login");
                     }}
-                    style={{fontSize: "15px"}}
+                    style={{ fontSize: "15px" }}
                   >
                     Cerrar sesión
                   </Link>
